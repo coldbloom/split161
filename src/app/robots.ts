04b4dcbbf8644390b1cad1f83f7794abe294e9b6
konstantin.kolesnik <Konstan.Kolesnik@x5.ru>
2025-07-18 03:19:44 +0300

    edit env robots anf sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    sitemap: `https://split161.vercel.app/sitemap.xml`,
   }
 }
